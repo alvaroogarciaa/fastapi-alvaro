a3596231d684f5ec5e2b74b7290d646466c855b3
alvaroogarciaa <alvaritogg07@gmail.com>
2025-01-14 17:00:18 +0100

    asasassasasas

--- fastapi/main.py
+++ fastapi/main.py
@@ -21,9 +21,9 @@ async def index(request: Request):
 async def frutas(request: Request):
     return templates.TemplateResponse("frutas.html", {"request": request})
 
-@app.get("/pescado", response_class=HTMLResponse)
+@app.get("/pescado-marisco", response_class=HTMLResponse)
 async def pescado(request: Request):
-    return templates.TemplateResponse("pescados.html", {"request": request})
+    return templates.TemplateResponse("pescados-mariscos.html", {"request": request})
 
 @app.get("/alcohol", response_class=HTMLResponse)
 async def alcohol(request: Request):
